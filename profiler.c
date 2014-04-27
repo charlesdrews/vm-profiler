5108770c4e28f1e5fadc80a0ff3bee3a5c0ec03b
Charles Drews <charlesdrews@gmail.com>
2014-04-27 16:31:46 -0400

    initial commit

---
+++ profiler.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+	printf("hello,");
+	bool b = true;
+	// use an if/else to check if latest
+	// vim indentation settings are working
+	if (b)
+		printf(" world\n");
+	else
+		printf("\n");
+    return 0;
+}
